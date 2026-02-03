c7a04d3b48aaaf6301f945bd98dfcf139234309d
fazli <fazli@knowpakistan.org>
2026-02-03 17:53:21 +0500

    change exe file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Relative base: assets load correctly at any URL (e.g. .../gym-landing-page/)
+  base: './',
 })
